168b241eff70a79deb8ded007750d490b960f555
Michael Valdron <14838510+michael-valdron@users.noreply.github.com>
2025-08-26 03:22:45 -0400

    go yaml parsing with maps

--- go/pkg/yaml/yaml.go
+++ go/pkg/yaml/yaml.go
@@ -2,17 +2,96 @@ package yaml
 
 import (
 	"os"
+	"reflect"
 
 	"gopkg.in/yaml.v3"
 )
 
-func parse(filename string) (*yaml.Node, error) {
-	var documentNode yaml.Node
+func Parse(filename string) (any, error) {
+	var document any
 	content, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
 
-	err = yaml.Unmarshal(content, &documentNode)
-	return &documentNode, err
+	err = yaml.Unmarshal(content, &document)
+	return document, err
+}
+
+func Depth(object any) int {
+	if object == nil {
+		return 0
+	}
+
+	value := reflect.ValueOf(object)
+
+	switch value.Kind() {
+	case reflect.Map:
+		maxDepth := 0
+		for _, key := range value.MapKeys() {
+			depth := Depth(value.MapIndex(key).Interface())
+			if depth > maxDepth {
+				maxDepth = depth
+			}
+		}
+		return maxDepth + 1
+	case reflect.Slice:
+		maxDepth := 0
+		for i := 0; i < value.Len(); i++ {
+			depth := Depth(value.Index(i).Interface())
+			if depth > maxDepth {
+				maxDepth = depth
+			}
+		}
+		return maxDepth + 1
+	default:
+		return 0
+	}
+}
+
+func Exists(object any, ids ...any) bool {
+	if len(ids) == 0 {
+		return true
+	} else if object == nil {
+		return false
+	}
+
+	value := reflect.ValueOf(object)
+
+	switch value.Kind() {
+	case reflect.Map:
+		idxValue := reflect.ValueOf(ids[0])
+		idxElement := value.MapIndex(idxValue)
+		if !idxElement.IsValid() {
+			return false
+		}
+		return Exists(idxElement.Interface(), ids[1:]...)
+	case reflect.Slice:
+		idxValue := ids[0].(int)
+		if idxValue >= value.Len() {
+			return false
+		}
+		return Exists(value.Index(idxValue).Interface(), ids[1:]...)
+	default:
+		return false
+	}
+}
+
+func Get(object any, ids ...any) any {
+	if object == nil || len(ids) == 0 {
+		return object
+	}
+
+	value := reflect.ValueOf(object)
+
+	switch value.Kind() {
+	case reflect.Map:
+		idxValue := reflect.ValueOf(ids[0])
+		return Get(value.MapIndex(idxValue).Interface(), ids[1:]...)
+	case reflect.Slice:
+		idxValue := ids[0].(int)
+		return Get(value.Index(idxValue).Interface(), ids[1:]...)
+	default:
+		return nil
+	}
 }
--- go/pkg/yaml/yaml_test.go
+++ go/pkg/yaml/yaml_test.go
@@ -1,54 +1,251 @@
 package yaml
 
 import (
-	"strings"
+	"reflect"
 	"testing"
-
-	"gopkg.in/yaml.v3"
 )
 
 func TestParse(t *testing.T) {
 	tests := []struct {
 		name              string
 		testfile          string
-		expectedTopLevels []string
+		expectedType      reflect.Kind
+		expectedTopLevels []any
 	}{
 		{
 			name:              "Test parsing",
 			testfile:          "../../resources/test/parse_test.yaml",
-			expectedTopLevels: []string{"metadata", "spec"},
+			expectedType:      reflect.Map,
+			expectedTopLevels: []any{"metadata", "spec"},
 		},
 	}
 
 	for _, testData := range tests {
 		t.Run(testData.name, func(tt *testing.T) {
-			topLevelLabels := make(map[string]bool)
-			documentNode, err := parse(testData.testfile)
+			document, err := Parse(testData.testfile)
 			if err != nil {
 				tt.Fatalf("Unexpected error parsing the yaml content on test %s: %v", testData.name, err)
 			}
 
-			if documentNode.Content[0].Kind == yaml.SequenceNode {
-				tt.Fatalf("Not expecting a sequence node")
+			documentObject := reflect.ValueOf(document)
+
+			if documentObject.Kind() != testData.expectedType {
+				tt.Fatalf("Expected result type: %v, Got result type: %v\n", testData.expectedType, documentObject.Kind())
 			}
 
-			for _, topLevel := range testData.expectedTopLevels {
-				topLevelLabels[topLevel] = false
-			}
+			for _, expectedTopLevel := range testData.expectedTopLevels {
+				switch documentObject.Kind() {
+				case reflect.Map:
+					key := reflect.ValueOf(expectedTopLevel)
+					child := documentObject.MapIndex(key)
+					if !child.IsValid() {
+						tt.Errorf("Expected %s was not found among roots", expectedTopLevel)
+					}
+				case reflect.Slice:
+					idx := reflect.ValueOf(expectedTopLevel)
+					if !idx.CanInt() {
+						tt.Fatalf("Unexpected slice where index is non-int")
+					}
 
-			for _, topLevel := range documentNode.Content[0].Content {
-				_, found := topLevelLabels[topLevel.Value]
-				if topLevel.Value != "" && !found {
-					tt.Errorf("%s is not in the list of expected roots: %s", topLevel.Value, strings.Join(testData.expectedTopLevels, ", "))
-				} else {
-					topLevelLabels[topLevel.Value] = true
+					if int(idx.Int()) >= documentObject.Len() {
+						tt.Errorf("Expected %d to be in the range of the top level list", idx.Int())
+					}
+				default:
+					if len(testData.expectedTopLevels) != 0 {
+						tt.Errorf("Was expected top level accessors to exists, got scalar instead.")
+					}
 				}
 			}
+		})
+	}
+}
 
-			for _, topLevel := range testData.expectedTopLevels {
-				if !topLevelLabels[topLevel] {
-					tt.Errorf("Expected %s was not found among roots", topLevel)
-				}
+func TestDepth(t *testing.T) {
+	tests := []struct {
+		name          string
+		input         any
+		expectedDepth int
+	}{
+		{
+			name:          "Test depth case 1",
+			input:         "test",
+			expectedDepth: 0,
+		},
+		{
+			name: "Test depth case 2",
+			input: map[string]any{
+				"name": "Example",
+				"spec": nil,
+			},
+			expectedDepth: 1,
+		},
+		{
+			name: "Test depth case 3",
+			input: map[string]any{
+				"metadata": map[string]any{
+					"name": "Example",
+				},
+				"spec": map[string]any{
+					"containers": []map[string]any{
+						{
+							"name":  "test-container",
+							"image": "docker.io/library/golang:1.24",
+						},
+					},
+				},
+			},
+			expectedDepth: 4,
+		},
+	}
+
+	for _, testData := range tests {
+		t.Run(testData.name, func(tt *testing.T) {
+			result := Depth(testData.input)
+
+			if result != testData.expectedDepth {
+				tt.Errorf("Expected: %d, Got: %d\n", testData.expectedDepth, result)
+			}
+		})
+	}
+}
+
+func TestExists(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    any
+		ids      []any
+		expected bool
+	}{
+		{
+			name:     "Test exists case 1",
+			input:    "test",
+			ids:      []any{0},
+			expected: false,
+		},
+		{
+			name: "Test exists case 2",
+			input: map[string]any{
+				"name": "Example",
+				"spec": nil,
+			},
+			ids:      []any{"spec"},
+			expected: true,
+		},
+		{
+			name: "Test exists case 3",
+			input: map[string]any{
+				"metadata": map[string]any{
+					"name": "Example",
+				},
+				"spec": map[string]any{
+					"containers": []map[string]any{
+						{
+							"name":  "test-container",
+							"image": "docker.io/library/golang:1.24",
+						},
+					},
+				},
+			},
+			ids:      []any{"spec", "containers", 0, "image"},
+			expected: true,
+		},
+		{
+			name: "Test exists case 4",
+			input: map[string]any{
+				"metadata": map[string]any{
+					"name": "Example",
+				},
+				"spec": map[string]any{
+					"containers": []map[string]any{
+						{
+							"name":  "test-container",
+							"image": "docker.io/library/golang:1.24",
+						},
+					},
+				},
+			},
+			ids:      []any{"spec", "containers", 0, "pullPolicy"},
+			expected: false,
+		},
+		{
+			name: "Test exists case 5",
+			input: map[string]any{
+				"metadata": map[string]any{
+					"name": "Example",
+				},
+				"spec": map[string]any{
+					"containers": []map[string]any{
+						{
+							"name":  "test-container",
+							"image": "docker.io/library/golang:1.24",
+						},
+					},
+				},
+			},
+			ids:      []any{"spec", "containers", 2, "image"},
+			expected: false,
+		},
+	}
+
+	for _, testData := range tests {
+		t.Run(testData.name, func(tt *testing.T) {
+			result := Exists(testData.input, testData.ids...)
+
+			if result != testData.expected {
+				tt.Errorf("Expected: %v, Got: %v\n", testData.expected, result)
+			}
+		})
+	}
+}
+
+func TestGet(t *testing.T) {
+	tests := []struct {
+		name          string
+		input         any
+		ids           []any
+		expectedValue any
+	}{
+		{
+			name:          "Test get case 1",
+			input:         "test",
+			ids:           []any{0},
+			expectedValue: nil,
+		},
+		{
+			name: "Test get case 2",
+			input: map[string]any{
+				"name": "Example",
+				"spec": nil,
+			},
+			ids:           []any{"spec"},
+			expectedValue: nil,
+		},
+		{
+			name: "Test get case 3",
+			input: map[string]any{
+				"metadata": map[string]any{
+					"name": "Example",
+				},
+				"spec": map[string]any{
+					"containers": []map[string]any{
+						{
+							"name":  "test-container",
+							"image": "docker.io/library/golang:1.24",
+						},
+					},
+				},
+			},
+			ids:           []any{"spec", "containers", 0, "image"},
+			expectedValue: "docker.io/library/golang:1.24",
+		},
+	}
+
+	for _, testData := range tests {
+		t.Run(testData.name, func(tt *testing.T) {
+			result := Get(testData.input, testData.ids...)
+
+			if result != testData.expectedValue {
+				tt.Errorf("Expected: %v, Got: %v\n", testData.expectedValue, result)
 			}
 		})
 	}
